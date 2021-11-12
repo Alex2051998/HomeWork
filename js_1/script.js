1edfd2ff5ac4abbb01ebc8b82e2f9902bac67579
Alex20511998 <Alex-2504@ukr.net>
2021-11-12 16:04:30 +0200

    test js

---
+++ js_1/script.js
@@ -0,0 +1,128 @@
+let a = 'Hello';
+let b = 'com';
+let c = 'ua';
+let d = 1;
+let e = 10;
+let f = -999;
+let g = 123;
+let h = 3.14;
+let i = 2.7;
+let j = 16;
+let k = true;
+let l = false;
+
+console.log(a,b,c,d,e,f,g,h,i,j,k,l);
+alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(i);
+document.write(j);
+document.write(k);
+document.write(l);
+
+
+a = 'Hello';
+b = 'A';
+c = 'l';
+d = 'e';
+e = 'x';
+f = '-';
+g = 25;
+h = '-';
+i = 4;
+j = '-';
+k = 1998;
+l = '!';
+
+console.log(a,b,c,d,e,f,g,h,i,j,k,l);
+alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(i);
+document.write(j);
+document.write(k);
+document.write(l);
+
+
+let firstName = 'Олександр ';
+let middleName = 'Олександрович ';
+let lastName = 'Фединець';
+let res;
+
+let person = firstName + middleName + lastName;
+
+console.log(person);
+
+let name = prompt();
+let fname = prompt();
+let old = prompt();
+
+console.log(res = "Вітаю " + name + ' ' + fname + " Тобі " + old);
+
+
+let m = 100;
+let n = '100';
+let o = true;
+
+console.log(typeof m);
+console.log(typeof n);
+console.log(typeof o);
+
+
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+console.log(5 === 6);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 != 10);
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(123 === '123');
+console.log(123 == '123');
+
+
+let str = "20";
+let result = 5;
+document.write("<br/>");
+document.write(str + result + "<br/>");
+document.write(str - result + "<br/>");
+document.write(str * "2" + "<br/>");
+document.write(str / 2 + "<br/>");
